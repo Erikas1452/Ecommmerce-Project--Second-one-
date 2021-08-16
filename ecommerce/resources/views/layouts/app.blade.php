2daeb4563ad89731a242f260e6db52606d527159
Erikas1452 <erikastumanovas@gmail.com>
2021-08-16 12:10:53 +0300

    Display product count next to cart instead of static data

--- ecommerce/resources/views/layouts/app.blade.php
+++ ecommerce/resources/views/layouts/app.blade.php
@@ -187,7 +187,7 @@
                                     <div class="cart_container d-flex flex-row align-items-center justify-content-end">
                                         <div class="cart_icon">
                                             <img src="{{ asset('public/frontend/images/cart.png') }}" alt="">
-                                            <div class="cart_count"><span>10</span></div>
+                                            <div class="cart_count"><span>{{ Cart::count() }}</span></div>
                                         </div>
                                         <div class="cart_content">
                                             <div class="cart_text"><a href="{{ route('show.cart') }}">Cart</a></div>
